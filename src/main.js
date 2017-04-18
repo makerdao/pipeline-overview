edb2be0cee1fd601cd3b2c8058e4840ce881a0b6
ferni <fertiberti@gmail.com>
2017-04-18 15:59:39 -0300

    Render each task in the appropriate column

--- src/main.js
+++ src/main.js
@@ -2,6 +2,23 @@ import "./styles/main.less";
 import Handlebars from 'handlebars';
 import data from './data.js';
 
+// Prints the task in the appropiate column according to the stage it is
+Handlebars.registerHelper('columnByStage', task => {
+	const stageIndex = data.stages.indexOf(task.stage);
+	if (stageIndex === -1) {
+		throw `Invalid stage: ${task.stage}`;
+	}
+	let html = '';
+	for (let i = 0; i < data.stages.length; i++) {
+		html += '<td>';
+		if (i === stageIndex) {
+			html += task.name;
+		}
+		html += '</td>';
+	}
+	return new Handlebars.SafeString(html);
+})
+
 document.addEventListener("DOMContentLoaded", () => {
 	const source   = document.getElementById("main-template").innerHTML;
 	const template = Handlebars.compile(source);
