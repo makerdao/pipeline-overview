68d998c0fd8064d9d9f9f79904f6d37029dda40e
ferni <fertiberti@gmail.com>
2017-04-25 23:19:32 -0300

    Slighly raise the background

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@ function draw(ctx) {
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width = '100%';
-  canvas.style.height = '80%';
+  canvas.style.height = '85%';
 
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
