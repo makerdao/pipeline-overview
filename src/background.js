ee475b892a45929c17df0560ddf3dabc254ac36a
ferni <fertiberti@gmail.com>
2017-04-25 22:41:33 -0300

    Animated background live

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@ function draw(ctx) {
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width = '100%';
-  canvas.style.height = '100%';
+  canvas.style.height = '80%';
 
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
@@ -14,52 +14,66 @@ function fitToContainer(canvas){
 
 const background = {
   start({colors}) {
-    const contexts = Array.from(document.querySelectorAll('.animated-background'))
-      .map(canvas => canvas.getContext('2d'));
-    const canvas = document.querySelector('.animated-background');
+    const targetCanvas = document.querySelector('.animated-background');
+    const targetCtx = targetCanvas.getContext('2d');
+    const modelCanvas = document.createElement("canvas");
+    const ctx = modelCanvas.getContext('2d');
     const lineSeparation = 2;
     const lineLength = 20;
     const lineWidth = 4;
-    const lines = colors.map((color, index) => ({
-      x: index * lineLength,
-      y: (index + 1) * (lineWidth + lineSeparation),
-      color
-    }));
+    const modelWidth = 50;
+    function generateLines(yOffset) {
+      return colors.map((color, index) => ({
+        x: Math.random() * modelWidth,
+        y: ((index + 1) * (lineWidth + lineSeparation)) + yOffset,
+        color,
+        speed: (Math.random() * 0.2) + 0.1
+      }));
+    }
 
+    const lines = generateLines(0).concat(generateLines((lineSeparation + lineWidth) * colors.length));
 
-    function draw(ctx) {
+    modelCanvas.width = modelWidth;
+    modelCanvas.height = (lineSeparation + lineWidth) * lines.length;
+
+    // Context global setup
+    ctx.globalAlpha = 0.8;
+    ctx.lineCap = "round";
+    ctx.lineWidth = lineWidth;
+
+    function draw() {
       ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-      ctx.fillStyle = 'gray';
-      ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+      targetCtx.clearRect(0, 0, targetCtx.canvas.width, targetCtx.canvas.height);
       lines.forEach(line => {
         ctx.beginPath();
         ctx.strokeStyle = line.color;
         ctx.moveTo(line.x, line.y);
         ctx.lineTo(line.x + lineLength, line.y);
+        // Draw behind for warping
+        ctx.moveTo(line.x - modelWidth, line.y);
+        ctx.lineTo(line.x - modelWidth + lineLength, line.y);
         ctx.stroke();
 
         // Update
-        line.x += 0.1;
-        if (line.x > ctx.canvas.width) {
-          line.x = -lineLength;
+        line.x += line.speed;
+        if (line.x > modelWidth) {
+          line.x = 0;
         }
       });
+
+      // Apply pattern to target canvas
+      targetCtx.fillStyle = targetCtx.createPattern(modelCanvas, "repeat");;
+      targetCtx.fillRect(0, 0, targetCanvas.width, targetCanvas.height);
+
+      window.requestAnimationFrame(draw);
     }
+    window.requestAnimationFrame(draw);
 
-    // Context global setup
-    contexts.forEach(ctx => {
-      ctx.globalAlpha = 0.8;
-      ctx.lineCap = "round";
-      ctx.lineWidth = lineWidth;
-    });
-
-    function drawAll() {
-      contexts.forEach(draw);
-      window.requestAnimationFrame(drawAll);
-    }
-    window.requestAnimationFrame(drawAll);
-
-    setInterval(() => fitToContainer(canvas), 200);
+    fitToContainer(targetCanvas);
+    document.body.onresize = function() {
+      console.log('resizee');
+      fitToContainer(targetCanvas);
+    };
   }
 }
 
