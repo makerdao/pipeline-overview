87937129471946fd379f9e05fb810ca96fc26cd9
ferni <fertiberti@gmail.com>
2017-04-26 18:28:56 -0300

    Background fills the top as well

--- src/background.js
+++ src/background.js
@@ -5,7 +5,7 @@ function draw(ctx) {
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width = '100%';
-  canvas.style.height = '85%';
+  canvas.style.height = '100%';
 
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
