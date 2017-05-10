33d151ba50435d21e08bb7813cee2d792a50ef27
Fernando Tiberti <fertiberti@gmail.com>
2017-05-10 17:54:53 -0300

    Background tweak

--- src/background.js
+++ src/background.js
@@ -1,7 +1,7 @@
 function fitToContainer(canvas){
   // Make it visually fill the positioned parent
   canvas.style.width = '100%';
-  canvas.style.height = '840px';
+  canvas.style.height = '100%';
 
   // ...then set the internal size to match
   canvas.width  = canvas.offsetWidth;
